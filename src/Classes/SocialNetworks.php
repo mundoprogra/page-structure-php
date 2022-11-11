37f8b860bcf38d8acd299ef63a27be94ffbd38ac
Alvar0RM <roma_971207@hotmail.com>
2022-11-11 16:40:53 -0600

    Se agrego al archivo para la gestion de las redes sociales la URL de Twitter, se agregarón comentarios a las funciones de todas las redes sociales

--- src/Classes/SocialNetworks.php
+++ src/Classes/SocialNetworks.php
@@ -4,10 +4,30 @@
 
   class SocialNetworks {
 
+    /**
+     * Función que regresa la URL de la red social Facebook.
+     * 
+     * @return string Regresa la url con la página de Facebook de la cuenta a la que pertenece.
+     */
     public static function getFacebook($fanPage) {
       return 'https://facebook.com/' . $fanPage;
     }
+
+    /**
+     * Función que regresa la URL de la red social Instagram.
+     * 
+     * @return string Regresa la url con la página de Instagram de la cuenta a la que pertenece.
+     */
     public static function getInstagram($account) {
       return 'https://instagram.com/' . $account;
+    }
+
+    /**
+     * Función que regresa la URL de la red social Twitter.
+     * 
+     * @return string Regresa la url con la página de Twitter de la cuenta a la que pertenece.
+     */
+    public static function getTwitter($account) {
+      return 'https://twitter.com/' . $account;
     }
   }
